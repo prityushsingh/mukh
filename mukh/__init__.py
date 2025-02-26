d9dde07433d310c9e0a59bec5076364f80a234dc
Ishan <ishandutta0098@gmail.com>
2025-02-26 23:43:20 +0530

    :hammer: [setup] Add dummy function

---
+++ mukh/__init__.py
@@ -0,0 +1,8 @@
+"""
+mukh - A python package to perform a variety of tasks on face images
+"""
+
+__version__ = "0.0.2"
+
+def open():
+    return "Hello World"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mukh",
-    version="0.0.1",
+    version="0.0.2",
     author="Ishan Dutta",
     author_email="duttaishan098@gmail.com",
     description="A python package to perform a variety of tasks on face images",
