00368f6663b88f17dfb6369c26e19c12ab891c8e
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-06-16 23:18:01 +0330

    :hammer: Update __init__.py to include all the modules (#7)

--- mukh/__init__.py
+++ mukh/__init__.py
@@ -5,5 +5,8 @@ mukh - A python package to perform a variety of tasks on face images
 __version__ = "0.1.14"
 
 __all__ = [
-    "detection",
+    "face_detection",
+    "deepfake_detection",
+    "reenactment",
+    "pipelines",
 ]
